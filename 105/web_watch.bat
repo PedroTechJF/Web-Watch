fa68bc90effd9290bc510771da48a9f08b245fc2
PedroTechJF <pedrotech.jf@gmail.com>
2026-06-23 12:13:23 -0300

    Ajustes Gerais

--- 105/web_watch.bat
+++ 105/web_watch.bat
@@ -17,13 +17,13 @@ timeout /t 2
 rmdir /q /s "D:\web_watch\"
 mkdir "D:\web_watch\"
 attrib +h +r "D:\web_watch"
-if exist "C:\Users\%User%\%AppName%" copy /Y "C:\Users\%User%\%AppName%" "D:\web_watch\"
+if exist "C:\Users\%username%\%AppName%" copy /Y "C:\Users\%username%\%AppName%" "D:\web_watch\"
 reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Web Watch" /f
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "Web Watch" /t REG_SZ /d "D:\web_watch\%AppName%" /f
 
-if exist "C:\Users\%User%\%AppName%" copy /Y "C:\Users\%User%\%AppName%" "D:\web_watch\"
+if exist "C:\Users\%username%\%AppName%" copy /Y "C:\Users\%username%\%AppName%" "D:\web_watch\"
 
-del /f /q "C:\Users\%User%\%AppName%"
+del /f /q "C:\Users\%username%\%AppName%"
 
 cd /d "D:\web_watch\"
 
--- 106/web_watch.bat
+++ 106/web_watch.bat
@@ -12,11 +12,11 @@ timeout /t 2
 rmdir /q /s "D:\web_watch\"
 mkdir "D:\web_watch\"
 attrib +h +r "D:\web_watch"
-if exist "C:\Users\%User%\%AppName%" copy /Y "C:\Users\%User%\%AppName%" "D:\web_watch\"
+if exist "C:\Users\%username%\%AppName%" copy /Y "C:\Users\%username%\%AppName%" "D:\web_watch\"
 del "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\web_watch.lnk"
 mklink "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\web_watch.lnk" "D:\web_watch\%AppName%"
 
-del /f /q "C:\Users\%User%\%AppName%"
+del /f /q "C:\Users\%username%\%AppName%"
 
 cd /d "D:\web_watch\"
 
